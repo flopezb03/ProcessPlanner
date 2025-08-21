c4a034f0ac23bbb611c9f90024a9bcac7ba6f830
flopezb03 <f.lopezberrocal@gmail.com>
2025-08-21 11:29:31 +0200

    reading input file added with little error handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 #include <fstream>
+#include <iostream>
 #include <regex>
 
 #include "fcfs_planner.h"
@@ -7,9 +8,11 @@
 
 using namespace std;
 
-void generator(const std::vector<std::pair<Process,int>>& vec, FcfsPlanner& planner) {
+void generator(std::queue<std::pair<Process,int>>& q, FcfsPlanner& planner) {
     int time = 0;
-    for (auto pair : vec) {
+    while (!q.empty()) {
+        auto pair = q.front();
+        q.pop();
         time = pair.second - time;
         std::this_thread::sleep_for(seconds{time});
         planner.insert_process(pair.first);
@@ -18,19 +21,44 @@ void generator(const std::vector<std::pair<Process,int>>& vec, FcfsPlanner& plan
 
 int main() {
 
-    std::vector<std::pair<Process,int>> vec;
-    vec.push_back({Process("P1",5),0});
-    vec.push_back({Process("P2",5),3});
-    vec.push_back({Process("P3",5),8});
+    //  Read input file
 
+    std::queue<std::pair<Process,int>> processes_queue;
+    ifstream processes_file;
+    try {
+        processes_file.open("../src/processes_input.txt");
+        if (!processes_file.is_open())
+            throw runtime_error("Could not open processes_input.txt");
+    }catch (exception& e) {
+        cerr << e.what() << endl;
+        return 1;
+    }
+
+    string line;
+    int line_index = 0;
+    while (getline(processes_file,line)) {
+        istringstream iss(line);
+        string p_name;
+        int p_duration;
+        int p_start;
+        if (!(iss >> p_name >> p_duration >> p_start)) {
+            cerr << "Invalid line: " << line_index << endl;
+        }
+        processes_queue.push({Process(p_name,p_duration),p_start});
+        line_index++;
+    }
+
+    // Create threads
 
     FcfsPlanner planner;
 
-    std::thread gen(generator, std::cref(vec), std::ref(planner));
-    std::thread t(&FcfsPlanner::execute_processes, &planner);
+    std::thread t_gen(generator, std::ref(processes_queue), std::ref(planner));
+    std::thread t_planner(&FcfsPlanner::execute_processes, &planner);
 
-    gen.join();
-    t.join();
+    // Join threads
+
+    t_gen.join();
+    t_planner.join();
 
     return 0;
 }
